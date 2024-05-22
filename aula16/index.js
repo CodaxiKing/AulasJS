b91402c7a99a63d0ba9bae2b8e2ef0024cb05708
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2024-05-22 14:41:08 -0300

    Add files via upload

---
+++ aula16/index.js
@@ -0,0 +1,28 @@
+//               0       1        2         3      4
+const alunos = ['Luiz', 'Maria', 'João'];// Luiza, Eduardo
+
+
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+
+//console.log(alunos.slice(0, 3));
+
+//delete alunos[1]; // o elemento 1 seria removido ficando vazio
+
+//alunos.shift(); // Remove do começo
+//alunos.pop(); // Remove o ultimo elemento
+//const removido = alunos.pop(); // estou salvando em uma variavel o nome que foi removido
+
+//alunos. unshift('Luiza'); // adiciona no começo
+//alunos. unshift('Fábio'); // No caso o Fábio ficará em primeiro, pois ele será executado depois
+
+//alunos[alunos.length] = 'Luiza'; // pegar o tamanho utilizando length
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Luana';
+
+//alunos[0] = 'Eduardo'; // alterar o elemento 0
+//alunos[3] = 'Luiza'; // Adicionar +1 elemento no caso o quarto nome
+
+//console.log(alunos);
+//console.log(alunos[1]);
