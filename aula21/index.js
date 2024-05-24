6a00f3ab68a63e1a040cc5e4b4851c2466c31b26
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2024-05-24 14:52:49 -0300

    Add files via upload

---
+++ aula21/index.js
@@ -0,0 +1,17 @@
+/* 
+Operadores de comparação
+
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== igualdade (valor) - não é recomendavél utilizar esse
+=== igualdade estrita (valor e tipo)
+!= Diferente (valor) - tbm n usa
+!== Diferente estrito (valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = 11;
+const comp = num1 !== num2;
+console.log(comp);
