b457dc87d7c476b9de51679322e8ba643aac3940
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2024-06-14 14:52:28 -0300

    Add files via upload

---
+++ aula22/index.js
@@ -0,0 +1,9 @@
+/*
+Operadores Lógicos
+&& -> AND -> E (Todas as expressões precisam ser verdadeiras para que AND retorne verdadeiro (Caso tenha 1 que n seja verdadeira, ele vai constar falso))
+|| -> OR -> OU (Todas as expressões precisam ser falsas para que o OR retorne falso (Caso tenha 1 que n seja falsa, ele vai constar verdadeiro))
+! -> NOT - > NÃO
+*/
+
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || true || false;
