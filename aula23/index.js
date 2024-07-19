f80fe3158ce5b526d2138a23da46c154a802a67e
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2024-07-19 09:50:57 -0300

    Add files via upload

---
+++ aula23/index.js
@@ -0,0 +1,23 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true && false -> Vai retoprnar "o valor verdadeiro"
+
+FALSY
+*false 
+0
+'' "" ``
+null / undefined
+NaN
+*/
+
+console.log (0 || false || null || 'Luiz otávio' || true);
+
+//console.log('Luiz Otávio' && undefined && 'Maria');
+
+// function falaOi () {
+//     return 'Oi';
+// };
+
+// let vaiExecutar = false;
+
+// console.log(vaiExecutar && falaOi());
