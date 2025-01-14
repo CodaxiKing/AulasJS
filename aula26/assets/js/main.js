4607040a1e09bc01640a107a71c2f9059099f5cf
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2025-01-14 16:28:50 -0300

    att

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -8,6 +8,7 @@ form.addEventListener('submit', function (e) {
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
+
     if (!peso) {
         setResultado ('Peso Inválido!', false);
         return;
@@ -18,12 +19,30 @@ form.addEventListener('submit', function (e) {
         return;
     }
 
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
 
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
 });
 
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc() {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
 function criaP (className) {
-const p = document.createElement('p');
-return p;
+    const p = document.createElement('p');
+    return p;
 }
 
 function setResultado (msg, isValid) {
@@ -31,5 +50,13 @@ function setResultado (msg, isValid) {
  resultado.innerHTML = '';
 
  const p = criaP();
+
+ if (isValid) {
+    p.classList.add('paragrafo-resultado');
+ } else {
+    p.classList.add('bad');
+ }
+
+ p.innerHTML = msg;
  resultado.appendChild(p)
 }
