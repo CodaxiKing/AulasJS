441e7a55644ee71ea435780062c488aa1b8708e4
Diego Tavares <107019645+CodaxiKing@users.noreply.github.com>
2025-02-04 14:36:48 -0300

    Create index.js

---
+++ aula27/index.js
@@ -0,0 +1,8 @@
+// ? :  Pontuação ternária
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'usuário Normal';
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preto';
+
+console.log(nivelUsuario);
